b8d17c1d9293e0230a10192ea03ab0331fbe2717
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-17 17:08:42 +0100

    updating loader to use _manifest files, if present

--- src/hemi/loader.rb
+++ src/hemi/loader.rb
@@ -1,8 +1,12 @@
 module Hemi
   module Loader
     def self.load_tree(tree_dir)
-      Dir[File.join(__dir__, tree_dir, "**", "*.rb")].sort.each do |file|
-        require file
+      if File.exist?(manifest_file = File.join(__dir__, tree_dir, "_manifest.rb"))
+        require manifest_file
+      else
+        Dir[File.join(__dir__, tree_dir, "**", "*.rb")].sort.each do |file|
+          require file
+        end
       end
     end
   end
