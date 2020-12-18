3da6ca4a4f0d122fe855cdf6a8f6a50bcf49612a
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-18 01:18:39 +0100

    Merging Font+Text classes, drying up with Texture parent

--- src/hemi/render/text.rb
+++ src/hemi/render/text.rb
@@ -1,58 +1,59 @@
 module Hemi::Render
-  class Text
-    ERR__INVALID_TEXTURE_SIZE = "Invalid texture size.".freeze
-    ERR__INVALID_POSITION     = "Ivalid position.".freeze
+  class Text < Texture
+    SDL2::TTF.init
 
-    def initialize
-      SDL2::TTF.init
+    ALLOWED_MODES = %i[solid blended shaded].freeze
+    FONT_FILE_PATTERN = /([\w-]*)_([\d]*)/.freeze
+
+    # ERR__INVALID_FONT_NAME = "Invalid font name format. :font_name_size expected".freeze
+    # ERR__INVALID_TEXTURE_SIZE = "Invalid texture size.".freeze
+    # ERR__INVALID_POSITION     = "Ivalid position.".freeze
+
+    @fonts = {}
+
+    def initialize(name_size)
+      name, size = *name_size.to_s.scan(FONT_FILE_PATTERN).flatten
+      @font_path = "assets/fonts/#{name}.ttf"
+      @font      = SDL2::TTF.open(font_path, size.to_i)
+
+      Text.register(name_size, self)
+    rescue SDL2::Error => _e
+      raise ArgumentError, format(ERR__FONT_NOT_FOUND, font_path)
     end
 
-    def render(font, text = nil, position: nil, mode: :blended)
-      texture       = Font[font].texturize(text, mode: mode)
-      font_texture  = Hemi::Window.renderer.create_texture_from(texture)
-      size          = calculate_size(font_texture, size)
-      position      = calculate_position(position)
-      texture_rect  = SDL2::Rect.new position.x, position.y, size.width, size.height
+    attr_reader :font_path, :size, :font
 
-      Hemi::Window.renderer.copy(font_texture, nil, texture_rect)
+    def render(text, position: nil, mode: :blended)
+      @texture = Hemi::Window
+                 .renderer
+                 .create_texture_from(surface(text, mode: mode))
+      size         = calculate_size(size)
+      position     = calculate_position(position)
+      texture_rect = SDL2::Rect.new position.x, position.y, size.width, size.height
+
+      Hemi::Window.renderer.copy(texture, nil, texture_rect)
+    end
+
+    class << self
+      attr_reader :fonts
+
+      def bucket
+        :fonts
+      end
     end
 
   private
 
-    def calculate_size(texture, size = nil)
-      size_obj = Size.new(height: nil, width: nil)
+    def surface(text, color: [255, 255, 255], mode: :blended, shade: [0, 0, 0])
+      raise(ERR__INVALID_MODE) unless ALLOWED_MODES.include? mode
 
-      if size.is_a? Array
-        size_obj.height = size[0]
-        size_obj.width  = size[1]
-      elsif size.is_a? Hash
-        size_obj.height = size[:height]
-        size_obj.width  = size[:width]
-      else
-        size_obj.height = texture.h
-        size_obj.width = texture.w
+      case mode
+      when :blended, :solid
+        font.send("render_#{mode}", text, color)
+      when :shaded
+        font.render_shaded(text, color, shade)
       end
-
-      size_obj
     end
 
-    def calculate_position(position = nil)
-      position_obj = Position.new(x: nil, y: nil)
-
-      if position.nil?
-        position_obj.x = 0
-        position_obj.y = 0
-      elsif position.is_a? Array
-        position_obj.x = position[0]
-        position_obj.y = position[1]
-      elsif position.is_a? Hash
-        position_obj.x = position[:x]
-        position_obj.y = position[:y]
-      else
-        raise ArgumentError(ERR__INVALID_POSITION)
-      end
-
-      position_obj
-    end
   end
 end
