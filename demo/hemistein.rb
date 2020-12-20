35e4d9ca9b8d8f9394f2efdb7bb30eded8cecd85
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-20 23:53:50 +0100

    ...create shortcuts for engine elements in top class

--- demo/hemistein.rb
+++ demo/hemistein.rb
@@ -3,6 +3,9 @@ require_relative "../src/hemi/engine"
 class Hemistein
   prepend Hemi::Engine
 
+  Font   = Hemi::Render::Font
+  Sprite = Hemi::Render::Sprite
+
   def initialize
     puts "Hemistein init"
   end
@@ -13,11 +16,11 @@ class Hemistein
 
   def loob_block
     proc {
-      Hemi::Render::Font[:jost_32].render("quick brown fox jumped over the lazy dog", position: [20, 20])
-      Hemi::Render::Font[:jost_16].render("quick brown fox jumped over the lazy dog", position: [20, 200])
+      Font[:jost_32].render("quick brown fox jumped over the lazy dog", position: [20, 20])
+      Font[:jost_16].render("quick brown fox jumped over the lazy dog", position: [20, 200])
 
-      Hemi::Render::Sprite[:gem].render(position: { y: 220, x: 20 })
-      Hemi::Render::Sprite[:gem].render(position: { y: 320, x: 220 }, size: { height: 64, width: 128 })
+      Sprite[:gem].render(position: { y: 220, x: 20 })
+      Sprite[:gem].render(position: { y: 320, x: 220 }, size: { height: 64, width: 128 })
     }
   end
 
