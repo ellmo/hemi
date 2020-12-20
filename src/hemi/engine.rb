893374030809c8114cce6f1874b054821667cca5
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-20 23:38:58 +0100

    Turning Engine class into a prependable module

--- src/hemi/engine.rb
+++ src/hemi/engine.rb
@@ -1,31 +1,38 @@
 require "singleton"
-require "forwardable"
 require "sdl2"
 
-require_relative "../helpers"
 require_relative "loader"
 require_relative "window"
 
 module Hemi
-  class Engine
-    include Singleton
-
+  module Engine
     @debug = false
 
     def initialize
       sdl_init
       load_trees
+      super
     end
 
-    attr_reader :window, :text, :image, :debug
+    attr_reader :window
 
-    def run
-      init_window
-      start_loop
+    def sdl_init
+      SDL2.init(SDL2::INIT_EVERYTHING)
+    end
+
+    def load_trees
+      Loader.load_tree "helpers"
+      Loader.load_tree "render"
+      Loader.load_tree "input"
+      Loader.load_tree "event"
     end
 
     class << self
       attr_reader :debug
+
+      def prepended(klass)
+        klass.include Singleton
+      end
 
       def debug_on!
         @debug = true
@@ -36,17 +43,13 @@ module Hemi
       end
     end
 
+    def run
+      super
+      init_window
+      start_loop
+    end
+
   private
-
-    def sdl_init
-      SDL2.init(SDL2::INIT_EVERYTHING)
-    end
-
-    def load_trees
-      Loader.load_tree "render"
-      Loader.load_tree "input"
-      Loader.load_tree "event"
-    end
 
     def init_window
       @window = Hemi::Window.instance
@@ -54,25 +57,6 @@ module Hemi
 
     def start_loop
       Hemi::Event::EventLoop.new.call(event_table, &loob_block)
-    end
-
-    def loob_block
-      proc {
-        Hemi::Render::Font[:jost_32].render("quick brown fox jumped over the lazy dog", position: [20, 20])
-        Hemi::Render::Font[:jost_16].render("quick brown fox jumped over the lazy dog", position: [20, 200])
-
-        Hemi::Render::Sprite[:gem].render(position: { y: 220, x: 20 })
-        Hemi::Render::Sprite[:gem].render(position: { y: 320, x: 220 }, size: { height: 64, width: 128 })
-      }
-    end
-
-    def event_table
-      {
-        keys: {
-          escape: -> { exit },
-          q: -> { exit }
-        }
-      }
     end
   end
 end
