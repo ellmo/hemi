c4e887f967d47dbb97b1ce82c77e10c3380ab5ce
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-21 02:43:20 +0100

    demonstrating how LoopMachine state switching works

--- demo/hemistein.rb
+++ demo/hemistein.rb
@@ -8,23 +8,40 @@ class Hemistein
   Sprite = Hemi::Render::Sprite
 
   def run
-    LM.register(:main, loob_block, event_table)
+    LM.register(:text, text_block, text_events)
+    LM.register(:image, sprite_block, sprite_events)
   end
 
-  def loob_block
+  def text_block
     proc {
       Font[:jost_32].render("quick brown fox jumped over the lazy dog", position: [20, 20])
       Font[:jost_16].render("quick brown fox jumped over the lazy dog", position: [20, 200])
-
-      Sprite[:gem].render(position: { y: 220, x: 20 })
-      Sprite[:gem].render(position: { y: 320, x: 220 }, size: { height: 64, width: 128 })
+      # LM.switch(:image)
     }
   end
 
-  def event_table
+  def sprite_block
+    proc {
+      Sprite[:gem].render(position: { y: 220, x: 20 })
+      Sprite[:gem].render(position: { y: 320, x: 220 }, size: { height: 64, width: 128 })
+      # LM.switch(:text)
+    }
+  end
+
+  def text_events
     {
       escape: -> { exit },
       q: -> { exit },
+      space: -> { LM.switch(:image) },
+      f12: :debug!
+    }
+  end
+
+  def sprite_events
+    {
+      escape: -> { exit },
+      q: -> { exit },
+      space: -> { LM.switch(:text) },
       f12: :debug!
     }
   end
