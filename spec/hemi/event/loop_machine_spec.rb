3a562c5c5c5beb35ece450ebcbbf864b2af02c09
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-21 17:05:54 +0100

    a bunch of simple specs

--- spec/hemi/event/loop_machine_spec.rb
+++ spec/hemi/event/loop_machine_spec.rb
@@ -1,5 +1,57 @@
 describe Hemi::Event::LoopMachine do
+  before { described_class.purge! }
 
-  pending 'NEEEEED SPECS'
+  describe ".new" do
+    subject { described_class.new }
 
+    it "class is a Singleton" do
+      expect(described_class.ancestors).to include Singleton
+    end
+
+    it "privatizes .new" do
+      expect { subject }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "initializaion" do
+    subject { described_class.instance }
+
+    it "does not allow to create new instances" do
+      expect(subject).to be_a described_class
+    end
+
+    it "starts with an empty loop list" do
+      expect(described_class.loops).to eq({})
+    end
+
+    it "has no current loop" do
+      expect(described_class.current).to be_nil
+    end
+  end
+
+  describe ".register" do
+    subject { described_class.register(*args) }
+
+    context "no other Loops present" do
+      let(:args) { [:main, {}, {}] }
+
+      it "registers a new Loop" do
+        expect { subject }
+          .to change(described_class.loops, :count)
+          .by(1)
+      end
+
+      it "sets up `current` loop" do
+        expect { subject }
+          .to change(described_class, :current)
+          .from(nil)
+      end
+
+      describe "loop fetching" do
+        it { expect(subject).to be_a Hemi::Event::EventLoop }
+        it { expect(subject).to eq described_class.current }
+        it { expect(subject).to eq described_class[:main] }
+      end
+    end
+  end
 end
