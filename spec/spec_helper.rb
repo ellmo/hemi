cd4383ae8f980c6af0dbd148b7ed09f5e9d0aab0
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-27 13:16:50 +0100

    adding fake classes to handle no-display systems
    (RSpec's doubles are losing their shit over here)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require "hemi"
+require "./spec/support/fake_display"
 
 class TestSuite
   prepend Hemi::Engine
+end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    # config.shared_context_metadata_behavior = :apply_to_host_groups
+    config.include FakeDisplay, fake_display: true
+    # config.include_context "fake display", fake_display: true
+  end
 end
