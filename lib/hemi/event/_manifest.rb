ea8a992718fcdfd03166cef56760d9b6ea0ddc39
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-29 17:14:48 +0100

    adding new Event::Pattern idea
    which is supposed to quickly match SDL2 events to actions

--- lib/hemi/event/_manifest.rb
+++ lib/hemi/event/_manifest.rb
@@ -1,3 +1,4 @@
 require_relative "key_handler"
+require_relative "pattern/key"
 require_relative "event_loop"
 require_relative "loop_machine"
---
+++ lib/hemi/event/pattern/key.rb
@@ -0,0 +1,38 @@
+module Hemi::Event
+  module Pattern
+    class Key
+      MATCHED_EVENT = "SDL2::Event::KeyDown".freeze
+
+      ALLOWED_TYPES = %i[up down].freeze
+      MOD_KEYS = %i[lshift rshift lctrl rctrl lalt ralt lgui rgui].freeze
+
+      def initialize(key, type: :down, mods: nil)
+        @type = type.to_s
+        @key  = key.to_s
+        @mods = mods
+        key_to_scancode
+        mods_to_modcode
+      end
+
+      attr_reader :type, :key, :mods, :scancode, :modcode
+
+      def hash
+        KeyPattern.new(scancode, modcode).hash
+      end
+
+      def inspect
+        "#<#{self.class}:#{hash} scancode=#{scancode} mod=#{modcode}>"
+      end
+
+      def key_to_scancode
+        @scancode = SDL2::Key::Scan.const_get(key.upcase)
+      end
+
+      def mods_to_modcode
+        @modcode = mods&.sum do |modkey|
+          SDL2::Key::Mod.const_get(modkey.upcase)
+        end || 0
+      end
+    end
+  end
+end
