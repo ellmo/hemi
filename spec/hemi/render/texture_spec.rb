c330227d5cd22e911fc1e07e84aab6b37661f966
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-30 19:50:56 +0100

    learning to write specs for abstracts

--- spec/hemi/render/texture_spec.rb
+++ spec/hemi/render/texture_spec.rb
@@ -7,8 +7,24 @@ describe Hemi::Render::Texture do
         def initialize; end
       end
 
+      subject { TextureBased.new }
+
       it "expects #render to be implemented" do
-        expect { TextureBased.new.render }.to raise_error(NotImplementedError)
+        expect { subject.render }.to raise_error(NotImplementedError)
+      end
+
+      it "expects .bucket to be implemented" do
+        expect { TextureBased.bucket }.to raise_error(NotImplementedError)
+      end
+
+      describe "#rectangle" do
+        it "calls SDL2::Rect.new" do
+          expect(subject).to(receive(:calculate_position))
+          expect(subject).to receive(:calculate_size)
+          expect(SDL2::Rect).to receive(:new)
+
+          subject.send :rectangle
+        end
       end
     end
   end
