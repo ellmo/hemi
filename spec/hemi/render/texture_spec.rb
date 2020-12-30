442df51f62068f491e5b363ca95f94033133e8ad
Jakub Żuchowski <ellmunzai@gmail.com>
2020-12-30 19:36:33 +0100

    attempting relatively clean inheritance test

--- spec/hemi/render/texture_spec.rb
+++ spec/hemi/render/texture_spec.rb
@@ -1,5 +1,15 @@
 describe Hemi::Render::Texture do
   describe "abstract class" do
     it { expect(described_class).to be_abstract }
+
+    context "inherited" do
+      class TextureBased < described_class # rubocop:disable RSpec/LeakyConstantDeclaration
+        def initialize; end
+      end
+
+      it "expects #render to be implemented" do
+        expect { TextureBased.new.render }.to raise_error(NotImplementedError)
+      end
+    end
   end
 end
